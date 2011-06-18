8e58e0ea8c430ec25409ba6d0c6ac2d2665f3bd1
Darrik Mazey <darrik@darmasoft.com>
2011-06-18 05:55:51 -0400

    search first for pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,10 +3,10 @@ class PagesController < ApplicationController
 	before_filter :load_scraper, :only => [:index]
 
 	def index
-		if !@scraper.nil?
-			@pages = @scraper.pages.order('scrape_ended_at asc, id asc').paginate(:per_page => 25, :page => params[:page])
-		elsif params[:search]
+		if params[:search]
 			@pages = Page.search(params[:search], :star => :true, :order => :scrape_ended_at, :sort_mode => :asc, :per_page => 25, :page => params[:page])
+		elsif !@scraper.nil?
+			@pages = @scraper.pages.order('scrape_ended_at asc, id asc').paginate(:per_page => 25, :page => params[:page])
 		else
 			@pages = Page.order('scrape_ended_at asc, id asc').paginate(:per_page => 25, :page => params[:page])
 		end
